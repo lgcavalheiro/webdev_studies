2d2a767b92bb3889066d88a809db1b1de0891d03
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-01-31 09:21:17 +0000

    studies

--- js_node/project/src/server.js
+++ js_node/project/src/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const port = 3003;
 const app = express();
 
-app.get('/products', (req, res, next) => {
+app.use((req, res, next) => {
     res.send({name: 'Notebook', price: 2499.99})
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
