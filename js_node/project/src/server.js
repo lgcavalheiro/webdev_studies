9800b2a1dbb02318d3bcf6541178422005eb5c6e
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-02-09 15:13:28 +0000

    studies for 09/02/2020

--- js_node/project/src/server.js
+++ js_node/project/src/server.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const port = 3003;
 const app = express();
+const database = require('./database.js');
+const bodyParser = require('body-parser');
 
-app.use((req, res, next) => {
-    res.send({name: 'Notebook', price: 2499.99})
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/products', (req, res) => {
+    res.send(database.getProducts())
+});
+
+app.get('/products/:id', (req, res) => {
+    res.send(database.getProduct(req.params.id))
+});
+
+app.post('/products', (req, res) => {
+    const product = database.saveProduct({
+        name: req.body.name,
+        price: req.body.price
+    });
+    res.send(product); //JSON
+});
+
+app.put('/products/:id', (req, res) => {
+    const product = database.saveProduct({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+    });
+    res.send(product); //JSON
+});
+
+app.delete('/products/:id', (req, res) => {
+    const product = database.deleteProduct(req.params.id);
+    res.send(product); //JSON
 });
 
 app.listen(port, () => {
