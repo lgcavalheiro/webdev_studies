e2f642c83b90c1db5a4975d2d5be6e107946c7a4
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-05-26 00:07:13 +0000

    more studies

--- ajax/server.js
+++ ajax/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const multer = require('multer');
 const app = express();
 
 app.use(express.static('.'));
@@ -7,4 +8,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/test', (req, res) => res.send('OK'));
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, '.')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+});
+
+const upload = multer({ storage }).single('arquivo');
+
+app.post('/upload', (req, res) => { 
+    upload(req, res, err => {
+        if(err) {
+            return res.end('An error ocurred')
+        }
+
+        res.end('File uploaded!')
+    }) 
+})
+
 app.listen(8080, () => console.log('Server is running on port 8080...'));
